358c45aa5e6db5d2b498ed85a073931bbac16251
exriba <44509247+exriba@users.noreply.github.com>
2024-04-09 04:01:36 -0400

    clean up
    Moved Equals and HashCode from Reward to base class as these methods are only using the Id field.

--- StreamDroid.Core/Common/EntityBase.cs
+++ StreamDroid.Core/Common/EntityBase.cs
@@ -7,5 +7,15 @@
     {
         /// TODO: Consider replacing with custom id class.
         public string Id { get; init; } = string.Empty;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj.GetType() != GetType()) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            var that = obj as EntityBase;
+            return Id.Equals(that.Id);
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
     }
 }
