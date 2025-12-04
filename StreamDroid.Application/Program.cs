e68894d5a2182e6c2d311431f505aa707e752b83
exriba <44509247+exriba@users.noreply.github.com>
2025-12-03 20:51:32 -0500

    clean up

--- StreamDroid.Application/Program.cs
+++ StreamDroid.Application/Program.cs
@@ -1,36 +1,31 @@
-using StreamDroid.Infrastructure;
-using StreamDroid.Domain;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using System.Net;
-using StreamDroid.Shared;
-using StreamDroid.Application.Settings;
-using StreamDroid.Application.Middleware;
-using StreamDroid.Application.API.Converters;
-using StreamDroid.Application;
 using Microsoft.Extensions.Options;
-using StreamDroid.Domain.Settings;
 using SharpTwitch.EventSub;
+using StreamDroid.Application;
+using StreamDroid.Application.API.Converters;
+using StreamDroid.Application.Middleware;
+using StreamDroid.Application.Settings;
+using StreamDroid.Domain;
+using StreamDroid.Domain.Settings;
+using StreamDroid.Infrastructure;
+using StreamDroid.Shared;
+using System.Net;
 
 #region Constants
 const string LOGOUT_PATH = "/logout";
 const string COOKIE_NAME = "StreamDroid";
 const string LOG4NET_CONFIG = "log4net.config";
-const string APP_SETTINGS_JSON = "appsettings.json";
 #endregion
 
 // StreamDroid.Application Configuration
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.ClearProviders();
 if (builder.Environment.IsDevelopment())
+{
     builder.Logging.AddConsole();
+}
 else
 {
-    var pathToExe = Environment.ProcessPath;
-    var pathToContentRoot = Path.GetDirectoryName(pathToExe);
-    Directory.SetCurrentDirectory(pathToContentRoot);
-    builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
-    builder.Configuration.AddJsonFile(APP_SETTINGS_JSON, optional: false, reloadOnChange: true);
     builder.Logging.AddLog4Net(LOG4NET_CONFIG, watch: true);
 }
 
@@ -52,18 +47,6 @@ builder.Services.AddTwitchEventSub();
 builder.Services.AddHttpClient();
 builder.Services.AddControllers()
                 .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new AssetConverter()));
-builder.Services.AddCors(options =>
-{
-    var appSettings = new AppSettings();
-    builder.Configuration.GetSection(AppSettings.Key).Bind(appSettings);
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins(appSettings.ClientUri)
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-    });
-});
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -75,7 +58,7 @@ builder.Services.AddAuthentication(options =>
     options.Cookie.Name = COOKIE_NAME;
     options.Events.OnRedirectToLogin = (context) =>
     {
-        context.Response.StatusCode = (int) HttpStatusCode.Unauthorized;
+        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
         return Task.CompletedTask;
     };
 });
@@ -89,15 +72,19 @@ builder.Services.AddMvc(options =>
 var app = builder.Build();
 // app.UseHttpsRedirection();
 app.UseRouting();
-app.UseCors();
 app.UseAuthentication();
 app.UseLocalFileServer();
 app.UseAuthorization();
 app.MapControllers();
+
 if (app.Environment.IsDevelopment())
+{
     app.UseDeveloperExceptionPage();
+}
 else
+{
     app.UseMiddleware<GlobalRequestHandler>();
+}
 #endregion
 
-app.Run();
+await app.RunAsync();
