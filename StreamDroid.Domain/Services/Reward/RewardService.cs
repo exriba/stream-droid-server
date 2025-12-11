2d2ef0b08f0a8e537157f29b51b391bb02453350
exriba <44509247+exriba@users.noreply.github.com>
2025-12-10 19:17:40 -0500

    Update RewardService.cs
    Switched to gRPC. All the logic will be handled in the domain layer.

--- StreamDroid.Domain/Services/Reward/RewardService.cs
+++ StreamDroid.Domain/Services/Reward/RewardService.cs
@@ -1,77 +1,245 @@
-﻿using Ardalis.GuardClauses;
-using Mapster;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
 using SharpTwitch.Core.Enums;
 using SharpTwitch.Helix;
 using StreamDroid.Core.Exceptions;
 using StreamDroid.Core.ValueObjects;
 using StreamDroid.Domain.DTOs;
+using StreamDroid.Domain.Services.AssetFile;
 using StreamDroid.Domain.Services.User;
 using StreamDroid.Infrastructure.Persistence;
+using static GrpcRewardService;
 using Entities = StreamDroid.Core.Entities;
 
 namespace StreamDroid.Domain.Services.Reward
 {
     /// <summary>
-    /// Default implementation of <see cref="IRewardService"/>.
+    /// Service class responsible for handling all Reward related logic.
     /// </summary>
-    public sealed class RewardService : IRewardService
+    [Authorize]
+    public sealed class RewardService : GrpcRewardServiceBase
     {
+        private const string ID = "Id";
+
         private readonly HelixApi _helixApi;
         private readonly IUserService _userService;
+        private readonly IAssetFileService _assetFileService;
         private readonly IRepository<Entities.Reward> _repository;
+        private readonly ILogger<RewardService> _logger;
 
-        public RewardService(HelixApi helixApi, 
-                             IUserService userService, 
-                             IRepository<Entities.Reward> repository)
+        public RewardService(HelixApi helixApi,
+                             IUserService userService,
+                             IRepository<Entities.Reward> repository,
+                             IAssetFileService assetFileService,
+                             ILogger<RewardService> logger)
         {
             _helixApi = helixApi;
             _repository = repository;
             _userService = userService;
+            _assetFileService = assetFileService;
+            _logger = logger;
         }
 
-        #region Rewards
-        /// <inheritdoc/>
-        public async Task<RewardDto> FindRewardByIdAsync(Guid rewardId)
+        /// <summary>
+        /// Finds a reward by the given id.
+        /// </summary>
+        /// <returns>A reward.</returns>
+        /// <exception cref="ArgumentException">If the reward id is an invalid GUID</exception>
+        public override async Task<RewardResponse> FindReward(RewardRequest request, ServerCallContext context)
         {
+            var rewardIdExists = Guid.TryParse(request.RewardId, out var rewardId);
+
+            if (!rewardIdExists || rewardId == Guid.Empty)
+                throw new ArgumentException($"Invalid Reward Id: {request.RewardId}.", nameof(request.RewardId));
+
             var reward = await FetchRewardAsync(rewardId);
-            return RewardDto.FromEntity(reward);
+
+            return new RewardResponse
+            {
+                Reward = RewardProto.FromEntity(reward),
+            };
         }
 
-        /// <inheritdoc/>
-        public async Task<IReadOnlyCollection<RewardDto>> FindRewardsByUserIdAsync(string userId)
+        /// <summary>
+        /// Finds a collection of rewards for the current user id.
+        /// </summary>
+        /// <returns>A collection of rewards.</returns>
+        public override async Task FindUserRewards(Empty request, IServerStreamWriter<RewardResponse> responseStream, ServerCallContext context)
         {
-            Guard.Against.NullOrWhiteSpace(userId, nameof(userId));
+            var userPrincipal = context.GetHttpContext().User;
+            var claim = userPrincipal.Claims.First(c => c.Type.Equals(ID));
 
-            var entities = await _repository.FindAsync(r => r.StreamerId.Equals(userId));
-            var rewards = entities.AsQueryable(); 
-            return rewards.ProjectToType<RewardDto>().ToList();
+            var rewards = await _repository.FindAsync(r => r.StreamerId.Equals(claim.Value));
+
+            if (rewards.Count == 0)
+            {
+                _logger.LogInformation("No rewards found. Searching external server.");
+                rewards = await SynchronizeRewardsAsync(claim.Value);
+            }
+
+            foreach (var reward in rewards)
+            {
+                var response = new RewardResponse
+                {
+                    Reward = RewardProto.FromEntity(reward)
+                };
+                await responseStream.WriteAsync(response);
+            }
         }
 
-        /// <inheritdoc/>
-        public async Task UpdateRewardSpeechAsync(Guid rewardId, Speech speech)
+        /// <summary>
+        /// Updates the speech for the given reward.
+        /// </summary>
+        /// <returns>A reward.</returns>
+        /// <exception cref="ArgumentException">If the reward id is an invalid GUID</exception>
+        public override async Task<RewardResponse> UpdateRewardSpeech(RewardSpeechRequest request, ServerCallContext context)
         {
+            var rewardIdExists = Guid.TryParse(request.RewardId, out var rewardId);
+
+            if (!rewardIdExists || rewardId == Guid.Empty)
+                throw new ArgumentException($"Invalid Reward Id: {request.RewardId}.", nameof(request.RewardId));
+
             var reward = await FetchRewardAsync(rewardId);
-            reward.Speech = speech;
+            reward.Speech = new Speech(enabled: request.Speech.Enabled, voiceIndex: request.Speech.VoiceIndex);
+            reward = await _repository.UpdateAsync(reward);
+
+            return new RewardResponse
+            {
+                Reward = RewardProto.FromEntity(reward),
+            };
+        }
+
+        /// <summary>
+        /// Adds assets to the given reward.
+        /// </summary>
+        /// <exception cref="ArgumentException">If the reward id is an invalid GUID</exception>
+        /// <returns>A reward.</returns>
+        public override async Task<RewardResponse> AddRewardAssets(IAsyncStreamReader<AddRewardAssetRequest> requestStream, ServerCallContext context)
+        {
+            var userPrincipal = context.GetHttpContext().User;
+            var claim = userPrincipal.Claims.First(c => c.Type.Equals(ID));
+
+            Entities.Reward reward;
+            var tasks = new List<Task>();
+            var hasFirstItem = await requestStream.MoveNext();
+
+            if (hasFirstItem)
+            {
+                var request = requestStream.Current;
+                var rewardIdExists = Guid.TryParse(request.RewardId, out var rewardId);
+
+                if (!rewardIdExists || rewardId == Guid.Empty)
+                    throw new ArgumentException($"Invalid Reward Id: {request.RewardId}.", nameof(request.RewardId));
+
+                reward = await FetchRewardAsync(rewardId);
+
+                reward.AddAsset(FileName.FromString(request.FileName), request.Volume);
+                var task = _assetFileService.AddAssetFilesAsync(claim.Value, reward.Title, FileName.FromString(request.FileName), request.File);
+                tasks.Add(task);
+
+                while (await requestStream.MoveNext())
+                {
+                    var current = requestStream.Current;
+                    reward.AddAsset(FileName.FromString(current.FileName), current.Volume);
+                    var innerTask = _assetFileService.AddAssetFilesAsync(claim.Value, reward.Title, FileName.FromString(current.FileName), current.File);
+                    tasks.Add(task);
+                }
+
+                await Task.WhenAll(tasks);
+                await _repository.UpdateAsync(reward);
+
+                return new RewardResponse
+                {
+                    Reward = RewardProto.FromEntity(reward)
+                };
+            }
+
+            return new RewardResponse();
+        }
+
+        /// <summary>
+        /// Updates assets from the given reward.
+        /// </summary>
+        /// <exception cref="ArgumentException">If the reward id is an invalid GUID</exception>
+        /// <returns>A reward.</returns>
+        public override async Task<RewardResponse> UpdateRewardAssets(UpdateRewardAssetRequest request, ServerCallContext context)
+        {
+            var rewardIdExists = Guid.TryParse(request.RewardId, out var rewardId);
+
+            if (!rewardIdExists || rewardId == Guid.Empty)
+                throw new ArgumentException($"Invalid Reward Id: {request.RewardId}.", nameof(request.RewardId));
+
+            var reward = await FetchRewardAsync(rewardId);
+            reward.RemoveAsset(request.FileName);
             await _repository.UpdateAsync(reward);
+            reward.AddAsset(FileName.FromString(request.FileName), request.Volume);
+            reward = await _repository.UpdateAsync(reward);
+
+            return new RewardResponse
+            {
+                Reward = RewardProto.FromEntity(reward)
+            };
         }
 
-        /// <inheritdoc/>
-        public async Task SynchronizeRewardsAsync(string userId)
+        /// <summary>
+        /// Removes assets from the given reward.
+        /// </summary>
+        /// <exception cref="ArgumentException">If the reward id is an invalid GUID</exception>
+        /// <returns>A reward.</returns>
+        public override async Task<RewardResponse> RemoveRewardAssets(RemoveRewardAssetRequest request, ServerCallContext context)
         {
+            var userPrincipal = context.GetHttpContext().User;
+            var claim = userPrincipal.Claims.First(c => c.Type.Equals(ID));
+
+            var rewardIdExists = Guid.TryParse(request.RewardId, out var rewardId);
+
+            if (!rewardIdExists || rewardId == Guid.Empty)
+                throw new ArgumentException($"Invalid Reward Id: {request.RewardId}.", nameof(request.RewardId));
+
+            var reward = await FetchRewardAsync(rewardId);
+
+            foreach (var fileName in request.FileName)
+            {
+                _assetFileService.DeleteAssetFile(claim.Value, reward.Title, FileName.FromString(fileName));
+                reward.RemoveAsset(fileName.ToString());
+            }
+
+            reward = await _repository.UpdateAsync(reward);
+
+            return new RewardResponse
+            {
+                Reward = RewardProto.FromEntity(reward)
+            };
+        }
+
+        /// <summary>
+        /// Synchronizes external rewards for the given user.
+        /// </summary>
+        /// <param name="userId">user id</param>
+        /// <returns>A collection of rewards.</returns>
+        private async Task<List<Entities.Reward>> SynchronizeRewardsAsync(string userId)
+        {
+            var rewards = new List<Entities.Reward>();
             var tokenRefreshPolicy = await _userService.CreateTokenRefreshPolicyAsync(userId);
 
             var twitchUsers = await tokenRefreshPolicy.Policy.ExecuteAsync(async context =>
-                await _helixApi.Users.GetUsersAsync(Array.Empty<string>(), tokenRefreshPolicy.AccessToken, CancellationToken.None), tokenRefreshPolicy.ContextData);
+                await _helixApi.Users.GetUsersAsync([], tokenRefreshPolicy.AccessToken, CancellationToken.None), tokenRefreshPolicy.ContextData);
 
             if (twitchUsers.Any())
             {
                 var twitchUser = twitchUsers.First();
+                _logger.LogInformation("Found user with id {id} and name {name}.", twitchUser.Id, twitchUser.DisplayName);
 
                 if (twitchUser.UserBroadcasterType is not BroadcasterType.NORMAL)
                 {
                     var twitchRewards = await _helixApi.CustomRewards.GetCustomRewardsAsync(userId, tokenRefreshPolicy.AccessToken, CancellationToken.None);
 
-                    var rewards = twitchRewards.Select(customReward =>
+                    if (twitchRewards.Any())
+                        _logger.LogInformation("Importing rewards.");
+
+                    var entities = twitchRewards.Select(customReward =>
                     {
                         var imageUrl = customReward.Image == null ? customReward.DefaultImage.Url1x : customReward.Image.Url1x;
                         return new Entities.Reward
@@ -86,61 +254,22 @@ namespace StreamDroid.Domain.Services.Reward
                         };
                     });
 
-                    foreach (var reward in rewards)
+                    foreach (var entity in entities)
                     {
-                        var rewardId = Guid.Parse(reward.Id);
-                        var rewardEntity = await FetchRewardByIdAsync(rewardId);
-
-                        if (rewardEntity is null)
-                            await _repository.AddAsync(reward);
-                        else
-                        {
-                            rewardEntity.Title = reward.Title;
-                            rewardEntity.Prompt = reward.Prompt;
-                            rewardEntity.ImageUrl = reward.ImageUrl;
-                            rewardEntity.BackgroundColor = reward.BackgroundColor;
-                            rewardEntity.Speech = new Speech(reward.Speech.Enabled, rewardEntity.Speech.VoiceIndex);
-                            await _repository.UpdateAsync(rewardEntity);
-                        }
+                        await _repository.AddAsync(entity);
                     }
+
+                    rewards.AddRange(entities);
+                    return rewards;
                 }
-            }
-        }
-        #endregion
 
-        #region Assets
-        /// <inheritdoc/>
-        public async Task<IReadOnlyCollection<Asset>> FindAssetsByRewardIdAsync(Guid rewardId)
-        {
-            var reward = await FetchRewardAsync(rewardId);
-            return reward.Assets;
-        }
-
-        /// <inheritdoc/>
-        public async Task<Tuple<string, IReadOnlyCollection<Asset>>> AddAssetsToRewardAsync(Guid rewardId, IDictionary<FileName, int> fileMap)
-        {
-            var reward = await FetchRewardAsync(rewardId);
-            var assets = new List<Asset>();
-
-            foreach (var entry in fileMap)
-            {
-                var asset = reward.AddAsset(entry.Key, entry.Value);
-                assets.Add(asset);
+                _logger.LogInformation("No rewards were found.");
+                return rewards;
             }
 
-            await _repository.UpdateAsync(reward);
-            return Tuple.Create<string, IReadOnlyCollection<Asset>>(reward.Title, assets);
+            _logger.LogInformation("Unable to find user in external server.");
+            return rewards;
         }
-
-        /// <inheritdoc/>
-        public async Task RemoveAssetsFromRewardAsync(Guid rewardId, IEnumerable<FileName> fileNames)
-        {
-            var reward = await FetchRewardAsync(rewardId);
-            foreach (var fileName in fileNames)
-                reward.RemoveAsset(fileName.ToString());
-            await _repository.UpdateAsync(reward);
-        }
-        #endregion
 
         #region Helpers
         /// <summary>
